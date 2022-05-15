60d2cb56e0c67c7fae3a544764c3614e8b1db47c
0sunset0 <chi6465618@naver.com>
2022-05-16 02:33:22 +0900

    commit

--- server.py
+++ server.py
@@ -6,25 +6,22 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/market-list')
-def market_list():
-    return render_template('market-list.html')
+@app.route('/market-list/<area>')
+def market_list(area):
+    return render_template('market-list.html', area=area)
 
-@app.route('/detail/<market_id>', methods=['GET'])
-def detail(market_id):
-    return render_template('detail.html', market_id=market_id)
+@app.route('/detail/<market>', methods=['GET'])
+def detail(market):
+    return render_template('detail.html', market=market)
 
-@app.route('/detail/<market_id>/create')
-def create(market_id):
-    return render_template('create.html', market_id=market_id)
+@app.route('/create/<market>')
+def create(market):
+    return render_template('create.html', market=market)
 
-@app.route('/detail/<market_id>/update')
-def update(market_id):
-    return render_template('update.html', market_id=market_id)
+@app.route('/update/<market>')
+def update(market):
+    return render_template('update.html', market=market)
 
-@app.route('/detail/<market_id>/delete')
-def delete(market_id):
-    return render_template('detail.html', market_id=market_id)
  
 if __name__ == '__main__':
     app.run(debug=True)
